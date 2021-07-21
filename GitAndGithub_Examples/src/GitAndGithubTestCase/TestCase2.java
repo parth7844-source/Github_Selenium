da182425ef13a0dff4c825951ee93107e8574073
parth7844-source <16mcal035,parth@gmail.com>
2021-07-21 12:09:40 +0530

    project code is created by parth

---
+++ GitAndGithub_Examples/src/GitAndGithubTestCase/TestCase2.java
@@ -0,0 +1,10 @@
+package GitAndGithubTestCase;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("system test case 2");
+	}
+	
+}
---
+++ GitAndGithub_Examples/src/GitAndGithubTestCase/Testcase1.java
@@ -0,0 +1,10 @@
+package GitAndGithubTestCase;
+
+public class Testcase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("system test case 1");
+	}
+
+}

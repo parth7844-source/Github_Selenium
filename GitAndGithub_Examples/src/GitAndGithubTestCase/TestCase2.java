9f0e4dfaf8dc2a4d0146131a7a2baa0b6389559f
parth7844-source <16mcal035,parth@gmail.com>
2021-07-21 13:15:23 +0530

    now parth change the code

--- GitAndGithub_Examples/src/GitAndGithubTestCase/TestCase2.java
+++ GitAndGithub_Examples/src/GitAndGithubTestCase/TestCase2.java
@@ -5,6 +5,7 @@ public class TestCase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("system test case 2");
+		System.out.println("New changes");
 	}
 	
 }
